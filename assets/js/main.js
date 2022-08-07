e7c3902b0e140ba6f2db39fce5f4fe3bef38ab71
nimaprmdi <nimaprmdi@gmail.com>
2022-08-07 18:04:01 +0430

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,8 +10,23 @@ jQuery(function ($) {});
   #Detect Element inside other element
 \*--------------------------------------*/
 function collaborate_ChildFinder(parentElement, childElement) {
-    let collaborate_result = document.querySelector(parentElement).getElementsByClassName(childElement)[0]
+    let collaborate_result = document
+        .querySelector(parentElement)
+        .getElementsByClassName(childElement)[0]
         ? true
         : false;
     return collaborate_result;
+}
+
+/*--------------------------------------*\
+  #Menu Toggle
+\*--------------------------------------*/
+if (collaborate_ChildFinder("body", "js-nav__toggle")) {
+    const menuButton = document.querySelector(".js-nav__toggle");
+    const nav = document.querySelector(".c-nav");
+
+    menuButton.addEventListener("click", function () {
+        menuButton.classList.toggle("on");
+        nav.classList.toggle("is-open");
+    });
 }
